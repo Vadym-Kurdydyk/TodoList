e12576b5a5d21a0934366a0ad97b4e690aab4d1a
Vadym Kurdydyk <vadimkurdidik@gmail.com>
2023-11-18 15:56:04 +0200

    Input and output from files added
    Made writing and reading from the file, but also made the code worse, next commit, gonna make id, description and done variables private again and make it look more appealing, maybe create some functions

--- todolistcpp/todolistcpp.cpp
+++ todolistcpp/todolistcpp.cpp
@@ -1,17 +1,16 @@
-// todolistcpp.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
 #include<string>
 #include<vector>
 #include <list>
 #include <ctime>
+#include <fstream>
 enum Choices {
     QUIT, ADD, COMPLETE
 };
 
 class Task {
-private:
+public:
     int id;
     std::string description;
     bool done;
@@ -38,55 +37,86 @@ int main()
     std::list<Task> Tasks;
     std::list<Task>::iterator it;
     Tasks.clear();
-    int choice, input_id;
+    int choice = ADD, input_id;
     std::string input_task;
     Task temp;
-        
-    while (1) {
-        system("cls");
-        std::cout << std::endl;
-        std::cout << std::endl;
+    std::fstream myfile;
+    myfile.open("tasks.txt", std::ios::out | std::ios::in);
+    char ch;
+
+
+    if (myfile.is_open()) {
+
+        while (!(myfile.eof())) {
+            Task task;
+            int id;
+            myfile >> task.id;
+            char separator;
+            myfile >> separator;
+            std::getline(myfile, task.description, '|');
+            myfile >> std::boolalpha >> task.done;
+            Tasks.push_back(task);
+            if (!(myfile >> task.id)) {
+                break;
+
+            }
+            myfile.close();
+        }
+        while (choice != QUIT) {
+            system("cls");
+            std::cout << std::endl;
+            std::cout << std::endl;
+
+
+            for (it = Tasks.begin(); it != Tasks.end(); it++) {
+
+                std::string iscompletedstr = it->iscompleted() ? "done" : "undone";
+
+                std::cout << it->getid() << " | " << it->getdescription() << " | " << iscompletedstr
+                    << std::endl;
+            }
+            if (Tasks.empty()) {
+                std::cout << "Add your first task!\n";
+            }
+            std::cout << "Press 1 to add task\n"
+                      << "Press 2 to comlete taks\n"
+                      << "Press 0 to quit\n";
+            std::cin >> choice;
+            switch (choice) {
+            case QUIT:
+                std::cout << "Have a great day\n";
+                break;
+            case ADD:
+                std::cout << "Enter a description of a task:\n";
+                std::cin.ignore();
+                getline(std::cin, input_task);
+                Tasks.push_back(Task(input_task));
+                continue;
+
+            case COMPLETE:
+                std::cout << "Enter id of a task you want to complete:\n";
+                std::cin >> input_id;
+                for (it = Tasks.begin(); it != Tasks.end(); it++) {
+
+                    if (input_id == it->getid()) {
+                        it->markcompleted(true);
+                        break;
+                    }
+                }
+                continue;
+            default:
+                std::cout << "Error, wrong input";
+                continue;
+            }
+
+        }
 
         for (it = Tasks.begin(); it != Tasks.end(); it++) {
 
-            std::string iscompletedstr = it->iscompleted() ? "done" : "undone";
+            myfile << it->getid() << " | " << it->getdescription() << " | " << it->iscompleted() << std::endl;
 
-            std::cout << it->getid() << " | " << it->getdescription() << " | " << iscompletedstr
-                      << std::endl;
         }
-        if (Tasks.empty()) {
-            std::cout << "Add your first task!\n";
-        }
-        std::cout << "Press 1 to add task\n"
-                  << "Press 2 to comlete taks\n"
-                  << "PRess 0 to quit\n";
-        std::cin >> choice;
-        switch (choice) {
-        case QUIT:
-            std::cout << "Have a great day\n";
-            return 0;
-        case ADD:
-            std::cout << "Enter a description of a task:\n";
-            std::cin.ignore();
-            getline(std::cin, input_task);
-            Tasks.push_back(Task(input_task));
-            continue;
-
-        case COMPLETE:
-            std::cout << "Enter id of a task you want to complete:\n";
-            std::cin >> input_id;
-            for (it = Tasks.begin(); it != Tasks.end(); it++) {
-
-                if (input_id == it->getid()) {
-                    it->markcompleted(true);
-                    break;
-                }
-            }
-            continue;
-        default:
-            std::cout << "Error, wrong input";
-            continue;
-        }
+        myfile.close();
     }
 }
 
